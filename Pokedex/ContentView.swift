b132d250624165ac72aa63fac72bd5b064fbd0fd
david De la Hoz <dricardelo@gmail.com>
2024-02-03 17:51:22 -0500

    Initial Commit

---
+++ Pokedex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by David De la Hoz on 3/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by David De la Hoz on 3/02/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
